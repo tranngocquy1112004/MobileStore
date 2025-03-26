933e93a360058d50b93df5da8d31d94831629240
tranngocquy1112004 <tranngocquy1818@gmail.com>
2025-03-26 22:26:28 +0700

    MobileStore

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProductPage from "./pages/ProductPage";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";import ProductPage from "./pages/ProductPage";
 import ProductDetail from "./components/ProductDetail";
 import Account from "./account/Account";
 import CartPage from "./pages/CartPage";
